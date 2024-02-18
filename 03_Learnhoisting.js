7636b5f53fd494e4102b2d9e492309932c9414e1
vidya1926 <83778108+vidya1926@users.noreply.github.com>
2024-02-18 18:41:15 +0530

    Add files via upload

---
+++ 03_Learnhoisting.js
@@ -0,0 +1,22 @@
+
+//let and var
+
+//Redeclaration 
+//Block scope of variable
+//Hoisting
+
+//Bring the content top
+
+//Bringing the declaration to the top
+
+// console.log(username);
+// var username='Testleaf';
+
+// var username;
+// console.log(username);
+// username='Testleaf';
+// console.log(username);
+
+
+console.log(username);
+const username='Testleaf';
